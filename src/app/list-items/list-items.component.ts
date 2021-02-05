a84b0536265203c0cd877acecc8c46cd0fcfe76a
Jared Bottelsen <jared.bottelsen@gmail.com>
2021-02-04 21:32:25 -0700

    Added list item edit functionality, added hover handling for buttons to hide and show as needed, added boolean values to initial list objects to aid in isolated CRUD operations

--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -7,46 +7,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemsComponent implements OnInit {
   titleNotAdded: boolean = false;
+  listItemNotAdded: boolean = false;
+  itemEditNotEntered: boolean = false;
+  editActive: boolean = false;
   lists = [];
-  isDisplay: boolean = false;
+  // isDisplay: boolean = false;
 
   addList(title: string) {
     if (title !== '') {
       this.lists.push({
         title: title,
-        items: [] 
+        items: [],
+        itemInputVisible: true
       })
       this.titleNotAdded = false 
+      console.log(this.lists);
     } else {
       this.titleNotAdded = true;
     }
-    console.log(this.lists);
   }  
 
-  addItem(item: string, index: number) {
+  addItem(item: string, listIndex: number) {
     if (item !== '') {
-      this.lists[index].items.push(item);
+      this.lists[listIndex].items.push({item: item, editEnable: false, showButtons: false});
       console.log(this.lists);
+      this.listItemNotAdded = false;
     } else {
       console.log('Add an item to the list thats not blank!');
+      this.listItemNotAdded = true;
     }
   }
 
+  showEdit(listIndex: number, itemIndex: number) {
+    this.lists[listIndex].items[itemIndex].editEnable = true;
+    this.lists[listIndex].itemInputVisible = false;
+  }
+
+  cancelEdit(listIndex: number, itemIndex: number) {
+    this.lists[listIndex].items[itemIndex].editEnable = false;
+    this.lists[listIndex].itemInputVisible = true;
+    this.lists[listIndex].items[itemIndex].showButtons = false;
+    this.itemEditNotEntered = false;
+  }
+
+  editItem(listIndex: number, itemIndex: number, itemEdit: string) {
+    if (itemEdit !== '') {
+      this.lists[listIndex].items[itemIndex].item = itemEdit;
+      this.lists[listIndex].items[itemIndex].editEnable = false;
+      this.lists[listIndex].items[itemIndex].showButtons = false;
+      this.lists[listIndex].itemInputVisible = true;
+      this.itemEditNotEntered = false;
+    } else {
+      console.log('Thats a blank edit my guy, make a true edit here!');
+      this.itemEditNotEntered = true;
+    }
+  } 
+
   deleteList(index: number) {
     this.lists.splice(index, 1);
-    console.log(this.lists);
   }
 
   deleteItem(listIndex: number, itemIndex: number) {
     this.lists[listIndex].items.splice(itemIndex, 1);
   }
 
-  mouseEnter(){
-    this.isDisplay = true;
+  mouseEnterDiv(listIndex: number, itemIndex: number){
+    this.lists[listIndex].items[itemIndex].showButtons = true;
  }
 
- mouseLeave(){
-    this.isDisplay = false;
+  mouseLeaveDiv(listIndex: number, itemIndex: number){
+    this.lists[listIndex].items[itemIndex].showButtons = false;
  }
 
  
